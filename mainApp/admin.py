135e31357107ee8650d3008ea4184ac8ae8d9501
abduvaliyevsalohiddin <abduvaliyevsalohiddin568@gmail.com>
2024-01-13 22:26:00 -0800

    5

--- mainApp/admin.py
+++ mainApp/admin.py
@@ -2,6 +2,23 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(Soha)
-admin.site.register(Savollar)
+
+@admin.register(Soha)
+class UstozAdmin(admin.ModelAdmin):
+    list_display = ["id", "nom"]
+    list_display_links = ["id", "nom"]
+    search_fields = ["nom"]
+    search_help_text = "Nom ustunlari bo'yicha qidiring"
+
+
+@admin.register(Savollar)
+class UstozAdmin(admin.ModelAdmin):
+    list_display = ["id", "nom", "variat", "soha"]
+    list_display_links = ["id", "nom"]
+    search_fields = ["nom"]
+    search_help_text = "Nom ustunlari bo'yicha qidiring"
+
+
+# admin.site.register(Soha)
+# admin.site.register(Savollar)
 admin.site.register(UserProfile)
